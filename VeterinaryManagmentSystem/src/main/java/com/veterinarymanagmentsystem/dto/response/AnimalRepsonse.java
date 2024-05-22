70bd73caa6ffd538ee213977bdf93ae300f7bbee
İsa Nezih Sayılırbaş <insbaglanti@gmail.com>
2024-05-22 16:47:53 +0300

    animal-dto

---
+++ VeterinaryManagmentSystem/src/main/java/com/veterinarymanagmentsystem/dto/response/AnimalRepsonse.java
@@ -0,0 +1,25 @@
+package com.veterinarymanagmentsystem.dto.response;
+
+import com.veterinarymanagmentsystem.entities.Animal;
+import com.veterinarymanagmentsystem.entities.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnimalRepsonse {
+    private Long id;
+    private String name;
+    private String species;
+    private String breed;
+    private String color;
+    private Animal.Gender gender;
+    private LocalDate birthDate;
+    private Customer customer;
+}
